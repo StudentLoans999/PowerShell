07d5ca45a0858e3b3d902a1d675688af75c50f7f
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-17 16:09:47 -0500

    Update RetryXNumberOfTimes.ps1

--- RetryXNumberOfTimes.ps1
+++ RetryXNumberOfTimes.ps1
@@ -9,3 +9,34 @@ DO
   $attemptsTried++
   
   try
+  {
+    Write-Host "Attempting to do Task" -ForegroundColor Green
+    
+    # Insert Task you want to get done write here
+    
+    # Make an error here to see how it handles it (only for testing purposes)
+    #Move-Item # this will cause an error if you uncomment this
+    
+    $successDetected = $true
+    $continueTrying = $false
+  }
+  catch
+  {
+    Write-Host "Failed to do Task" -ForegroundColor Green
+    
+    Write-Host "attemptsAllowed = " $attemptsAllowed
+    Write-Host "continueTrying = " $continueTrying
+    Write-Host "attemptsTried = " $attemptsTried
+    Write-Host "successDetected = " $successDetected
+  }
+  $attemptsAllowed--
+} # end of Do loop
+while (($continueTrying) -and ($attemptsAllowed -gt 0)) # Run until it either the Task is run successfully or until there are no Attempts left
+
+# Ran out of Attempts and Task still didn't run successfully
+if (($attemptsAllowed -eq 0) -and ($successDetected -eq $false)) { Write-Host "Task failed after " $attemptsTried " unsuccessful attempts." -ForegroundColor Yellow }
+
+  Write-Host "attemptsAllowed = " $attemptsAllowed
+  Write-Host "continueTrying = " $continueTrying
+  Write-Host "attemptsTried = " $attemptsTried
+  Write-Host "successDetected = " $successDetected
