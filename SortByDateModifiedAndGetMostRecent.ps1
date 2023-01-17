8b82c5d6e293528a619fdb8fa5f9f2e5f9c5d76f
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-17 16:29:25 -0500

    Update SortByDateModifiedAndGetMostRecent.ps1

--- SortByDateModifiedAndGetMostRecent.ps1
+++ SortByDateModifiedAndGetMostRecent.ps1
@@ -1,1 +1,13 @@
-$folderPath = "\\spb-ABC01
+$folderPath = "\\ABC-app01\Public\Data\Files"
+$fileFilter = "*ABC*.csv*
+$filesLookingFor = (Get-ChildItem -Path $folderPath -Filter fileFilter) # could also type "gci" instead of "Get-ChildItem"
+
+$OFS = "`r`n`r`n" # this will add a line betweeen each Write-Host result for clear formatting
+
+$latestFile = $filesLookingFor | Sort-Object LastWriteTime -Descending | Select-Object -Index 0
+$lastModTime = $latestFile.LastWriteTime
+
+Write-Host `n "Here is the list of files found, ordered with the most Recent at the bottom:"`n -BackgroundColor Black
+Write-Host $filesLookingFor`n -ForegroundColor Green
+Write-Host "Here is the most Recent file:"`n
+Write-Host $latestFile "Last Write TimeL $lastModTime" `n -ForegroundColor Yellow
