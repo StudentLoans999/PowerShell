898f2f83ab7dc3409c8a3b2d336d249f2c61ad12
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-18 13:59:08 -0500

    Update CreateCSVFileFromTXT.ps1

--- CreateCSVFileFromTXT.ps1
+++ CreateCSVFileFromTXT.ps1
@@ -1,4 +1,16 @@
-$files = Get-ChildItem "\\ABC-app01\Public\Data\Files\*ABC*.txt"
+$filesLookingFor = Get-ChildItem "\\ABC-app01\Public\Data\Files\*ABC*.txt" # can also use "gci" isntead of "Get-ChildItem"
 $count = 1
 
-foreach($file in $file
+ForEach ($file in $filesLookingFor)
+{
+  $contents = import-csv $file -Delimiter "`t"
+  $newName = [System.IO.Path]::GetDirectoryName($file.FullName) + "\" + "DEF.csv" -f (Get-Date).ToString("yyyyMMdd_HHmmss"), $count
+  
+  ForEach ($line in $contents) { $line.Day_Code = (Get-Date $line.Day_Code).ToString("yyyy-MM-dd") }
+  
+  $contents | export-csv -Path ($newName + "_1") -Delimiter "," -NoClobber -NoTypeInformation
+  
+  Get-Content ($newName + "_1") | % { $_ -replace '"','' } | Set-Content $newName
+  
+  $count += 1
+}
