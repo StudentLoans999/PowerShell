0f263ea1767e59bae1f2ebd2e5c3279841013f63
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-18 10:20:13 -0500

    Update MakeTempFolder.ps1

--- MakeTempFolder.ps1
+++ MakeTempFolder.ps1
@@ -1,2 +1,27 @@
 # Creating a function since this is called upon often 
-function DR-Get-DaysToX
+function DR-Make-Temp-Folder
+{
+  param
+  {
+    $rootFolder = $PSScriptRoot,
+    $folderPrefix = 'temp_'
+  } # can change the $rootFolder to be a new variable you created in the line right before, to change the path
+    # EX: Line A: $newPath = $PSScriptRoot + "\ABC"
+    # EX: Line B: $rootFolder = $newPath
+  
+  $tempFolderName = $folderPrefix + (Get-Date -Format "yyyyMMddHHmmss")
+  $testThis = Test-Path -Path ($rootFolder + $tempFolderName)
+  
+  if (!$testThis)
+  {
+    New-Item -Path $rootFolder -Name $tempFolderName -ItemType "directory"
+    ($rootFolder + $tempFolderName)
+    return
+  }
+  
+  else # returns $false if the folder already exists 
+  {
+    $false
+    return
+  }
+}
