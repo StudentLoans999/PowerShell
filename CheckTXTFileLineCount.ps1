44be2f2d383f6f3d3efb661aef9fecbf3b46c7f3
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-18 15:09:15 -0500

    Update CheckTXTFileLineCount.ps1

--- CheckTXTFileLineCount.ps1
+++ CheckTXTFileLineCount.ps1
@@ -1,2 +1,20 @@
-# This checks for TXT files that are less than 10 Lines long
+# This checks for TXT files that are less than 5 Lines long, if so, then the file gets deleted, if not, do X process
 $checkThisFolder = "\\ABC-app01\Public\Data\Files\CheckForLength"
+$fileFilter = "*ABC*.txt"
+
+Get-ChildItem -Path $checkThisFolder -Filter $fileFilter | Foreach-Object
+{
+  $theCount = (Get-Content $_.FullName | Measure-Object -Line) # could do other things with Measure-Object like -Characters
+  
+  if ($theCount.Lines -lt 5)
+  {
+    # Delete the nearly empty file:
+    Write-Output "Deleting " + $_.FullName
+    Remove-Item -Path $_.FullName
+  }
+  
+  else
+  {
+  # Do X process here if the file has more than 4 lines in it
+  }
+}
