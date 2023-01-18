a6a3d251a42b3b856976485216c2779f26a04bae
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-18 14:58:03 -0500

    Update LimitXFileProcess.ps1

--- LimitXFileProcess.ps1
+++ LimitXFileProcess.ps1
@@ -1,2 +1,15 @@
-# This script limits how many times a process (X) gets run. In this case it is moving files (so it limits how many files get moved at once)
-$checkThisFolder = 
+# This script limits how many times a process (X) gets run. In this case it is moving files (so only 5 files get moved at once)
+$checkThisFolder = "\\ABC-app01\Public\Data\Files\Done"
+$fileFilter = "*ABC*.xlsx"
+$targetFolder = "\\ABC-app01\Public\Data\Files\"
+$numberToLimit = 5
+$thisFileNumber = 1
+
+Get-ChildItem - Path $checkThisFolder -Filter $fileFilter | Foreach-Object
+{
+  if ($thisFileNumber -le $numberToLimit)
+  {
+    Move-item -Path $_.FullName -Destination $targetFolder # here you can do whatever process you want limited
+    $thisFileNumber++
+  }
+}
