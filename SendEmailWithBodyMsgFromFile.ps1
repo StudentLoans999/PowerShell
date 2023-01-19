fb5d8f441667521f37834252b39987ae315afda1
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-19 11:38:41 -0500

    Update SendEmailWithBodyMsgFromFile.ps1

--- SendEmailWithBodyMsgFromFile.ps1
+++ SendEmailWithBodyMsgFromFile.ps1
@@ -1,3 +1,21 @@
+# This script sends out an email with it's message coming from a TXT file
+
 $date = Get-Date -Format yyyy-MM-dd
+$username = "david_richey@abc.com"
+$sendTo = "john_smith@def.com"
 
 #readhost -assecurestring | convertfrom-securestring | out-file 
+
+# Extract the password from the password file
+$password = get-content "\\ABC-app01\Public\Creds\ABCCredentials.txt" | convertto-securestring
+$credentials = new-object -typename System.Management.Automation.PSCredential -argumentlsit $username, $password
+
+$body = [string]::Join("`n", (Get-Content "\\ABC-app01\Public\Data\Emails\ABCEmail.txt")) # set the contents of the email to be from a TXT file
+
+# Send the email with these configurable attributes
+Send-MailMessage `
+-Body $body -BodyAsHtml `
+-Subject "$date - ABC's Reminder to you: don't worry, be happy" `
+-Credential $credentials `
+-SmtpServer "smtp-mail.outlook.com" -UseSsl `
+-From $username -To $sendTo
