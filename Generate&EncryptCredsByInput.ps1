2ed198fa39a60b652043ed1c707a112f59190edb
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-19 10:07:57 -0500

    Update and rename GenerateCredsByInput.ps1 to Generate&EncryptCredsByInput.ps1

--- Generate&EncryptCredsByInput.ps1
+++ Generate&EncryptCredsByInput.ps1
@@ -1,4 +1,4 @@
-# This script allows user to input a new/updated password when ran, that will update the txt file which is a hash of the password, with a new hash 
+# This script allows user to input a new/updated (unecnryoted) password when ran, that will update the txt file which is a hash (encryuption) of the password, with a new hash 
 $path = "\\ABC-app01\Public\Creds\ABCCredentials.txt"
 read-host -assecurestring | convertfrom-securestring | out-file $path
 # EX: user inputs "password123" after running the script, well then in ABCCredentials.txt whatever the password was before, it is now a hash of password123
