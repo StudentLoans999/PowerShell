763eab862864cd00c7493ffda18721569a769829
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-19 10:01:35 -0500

    Update GenerateCredsByInput.ps1

--- GenerateCredsByInput.ps1
+++ GenerateCredsByInput.ps1
@@ -1,1 +1,4 @@
-$path = "
+# This script allows user to input a new/updated password when ran, that will update the txt file which is a hash of the password, with a new hash 
+$path = "\\ABC-app01\Public\Creds\ABCCredentials.txt"
+read-host -assecurestring | convertfrom-securestring | out-file path
+# EX: user inputs "password123" after running the script, well then in ABCCredentials.txt whatever the password was before, it is now a hash of password123
