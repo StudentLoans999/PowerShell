4cf1a06ae7dd268a09f6fa33a7347b13da13af1c
David Richey <davidsacc@gmail.com>
2023-01-27 13:54:38 -0500

    Update SplitXLSXFileIntoXNumberOfTXTFiles.ps1

--- SplitXLSXFileIntoXNumberOfTXTFiles.ps1
+++ SplitXLSXFileIntoXNumberOfTXTFiles.ps1
@@ -1,9 +1,9 @@
 # This script will make an array of Months and will split an xlsx file that contains 12 sheets (12 months) into 12 separate txt files, each one being only one sheet (a month), and will archive the original
 # EX: Make an array (substring will be added to beginning of each filename), scripts looks for a file in the path, and then splits that into X txt files, each one being one sheet
 
-$checkThisFolder = "\\ABC-app01\Public\Data\Files\"
-$archiveFolder = "\\ABC-app01\Public\Data\Files\Archive\"
-$targetFolder = "\\ABC-app01\Public\Data\Files\SplitIntoX"
+$checkThisFolder = "\\ABC-server\Public\Data\Files\"
+$archiveFolder = "\\ABC-server\Public\Data\Files\Archive\"
+$targetFolder = "\\ABC-server\Public\Data\Files\SplitIntoX"
 
 $xArray = @('JAN_', 'FEB_', 'MAR_', 'APR_', 'MAY_', 'JUN_', 'JUL_', 'AUG_', 'SEP_', 'OCT_', 'NOV_', 'DEC_') # change this to whatever type of array you want
 $fileFilter = '*ABC*.xlsx'
@@ -15,7 +15,7 @@ ForEach ($thisFile in $files)
   {
     $oldName = $thisFile.Name
     Copy-item -Path $thisFile.FullName -Destination "$targetFolder\$thisX$oldName" -Force # Creates a copy of the xlsx file to the path the flattener will look for it
-    $flattenedCommand = '& "\\ABC-app01\Public\Apps\Flattener.exe" + $thisX.substring(0,3) + '.abc' #.substring is 0 and 3 because each item in the array is 4 characters
+    $flattenedCommand = '& "\\ABC-server\Public\Apps\Flattener.exe" + $thisX.substring(0,3) + '.abc' #.substring is 0 and 3 because each item in the array is 4 characters
     
     try
     {
