9b0f9a90169b8edf18e4a779b0aacef063999f19
David Richey <davidsacc@gmail.com>
2023-02-09 10:05:03 -0500

    Update MakeTempFolder.ps1

--- MakeTempFolder.ps1
+++ MakeTempFolder.ps1
@@ -1,17 +1,17 @@
-# Creating a function since this is called upon often 
+# Creating a function since this is called upon often - this creates a folder named "temp_date"
 function DR-Make-Temp-Folder
 {
-  param
-  {
-    $rootFolder = $PSScriptRoot,
+    #$newPath = "C:\new"
+    $rootFolder = $PSScriptRoot #$newPath
     $folderPrefix = 'temp_'
-  } # can change the $rootFolder to be a new variable you created in the line right before, to change the path
+    # can change the $rootFolder to be a new variable you created in the line right before, to change the path
     # EX: Line A: $newPath = $PSScriptRoot + "\ABC"
     # EX: Line B: $rootFolder = $newPath
   
   $tempFolderName = $folderPrefix + (Get-Date -Format "yyyyMMddHHmmss")
   $testThis = Test-Path -Path ($rootFolder + $tempFolderName)
   
+  # Create the folder
   if (!$testThis)
   {
     New-Item -Path $rootFolder -Name $tempFolderName -ItemType "directory"
@@ -25,3 +25,14 @@ function DR-Make-Temp-Folder
     return
   }
 }
+
+# Run the function created above
+$theTempPath = $false
+while (!$theTempPath)
+{
+    $theTempPath = DR-Make-Temp-Folder -RootFolder $h -FolderPrefix 'temp_'
+}
+
+# Get info about new folder created
+$theTempPath
+Write-Host Test-Path -Path $theTempPath
