7f03d5995165759906f35197ec59c435dddcc828
David Richey <davidsacc@gmail.com>
2023-05-08 19:43:21 -0400

    Create Sort&RenameByDate.ps1

---
+++ Sort&RenameByDate.ps1
@@ -0,0 +1,15 @@
+$folderPath = ""
+$fileFilter = "*.txt"
+
+# Get all filtered files in folder path provided above
+$filesLookingFor = (Get-ChildItem -Path $folderPath -Filter $fileFilter) | Sort-Object lastwritetime # sorts files found in folder by modified date, descending
+
+$ie = 1
+
+ForEach ($theFile in $filesLookingFor)
+{
+  Try
+  {
+    $newName = ($ie.ToString("0000")+"_"+$theFile.Name)
+    Write-Host "Renaming to: $newName "
+    Rename-Item -Path 
