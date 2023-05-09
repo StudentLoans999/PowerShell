9a380f759564850c86049e48d45d3f18d3fa3b7d
David Richey <davidsacc@gmail.com>
2023-05-09 11:05:23 -0400

    Update Sort&RenameByDate.ps1

--- Sort&RenameByDate.ps1
+++ Sort&RenameByDate.ps1
@@ -1,3 +1,5 @@
+# Sorts files found in folder by modified date, descending, and then renames each file with a number starting with 1 and also adds a _ at the front of the filename 
+
 $folderPath = ""
 $fileFilter = "*.txt"
 
@@ -8,9 +10,9 @@ $ie = 1
 
 ForEach ($theFile in $filesLookingFor)
 {
-  Try
+  Try # Renames files with previous number + 1
   {
-    $newName = ($ie.ToString("0000")+"_"+$theFile.Name)
+    $newName = ($ie.ToString("0000")+"_"+$theFile.Name) # uses .ToString("0000") since 0001 evnentually goes to 0010 for the second digit which is right, but not converting it would be 1 going to 10 with could be a problem with software automation
     Write-Host "Renaming to: $newName "
     Rename-Item -Path $folderPath$theFile $newName
     $ie += 1
